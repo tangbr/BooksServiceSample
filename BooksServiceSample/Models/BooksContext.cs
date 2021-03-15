2981fd17857e000377c20c1b66d9adfc08d08b0f
tangbr <brucetang6@gmail.com>
2021-03-15 00:09:06 -0400

    Add project files.

---
+++ BooksServiceSample/Models/BooksContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BooksServiceSample.Models
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions<BooksContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookChapter> Chapters { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookChapter>()
+                .ToTable("Chapters")
+                .HasKey(c => c.Id);
+            modelBuilder.Entity<BookChapter>()
+                .Property(c => c.Id)
+                .HasColumnType("UniqueIdentifier")
+                .HasDefaultValueSql("newid()");
+            modelBuilder.Entity<BookChapter>()
+                .Property(c => c.Title)
+                .HasMaxLength(120);
+                
+        }
+    }
+}
---
+++ BooksServiceSampleHost/Migrations/20210314172151_NewMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BooksServiceSampleHost.Migrations
+{
+    public partial class NewMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Chapters",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "UniqueIdentifier", nullable: false, defaultValueSql: "newid()"),
+                    Number = table.Column<int>(nullable: false),
+                    Pages = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(maxLength: 120, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chapters", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Chapters");
+        }
+    }
+}
